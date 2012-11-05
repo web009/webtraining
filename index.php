b438c7a6f5c09d33c29a96547c20f11683d6d73a
hisako kanemoto <kanemoto.hisako.tci@gmail.com>
2012-11-05 13:50:45 +0900

    ファイル追加

---
+++ index.php
@@ -0,0 +1,143 @@
+<?php require('wordpress/wp-load.php'); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!-- saved from url=(0014)about:internet -->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>サイト.png</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<style type="text/css">
+td img {display: block;}body {
+}
+
+#wrapper {
+	width: 960px;
+	margin-right: auto;
+	margin-left: auto;
+}
+#contents{
+	position: relative;
+}
+#whats {
+	height: 90px;
+	width: 230px;
+	position: absolute;
+	top: 500px;
+	left: 110px;
+	border-style: none;
+	font-size: 19px;
+	list-style: none;
+}
+#blog {
+	height: 190px;
+	width: 230px;
+	position: absolute;
+	top: 530px;
+	left: 83px;
+	border-style: none;
+	font-size: 12px;
+	list-style: none;
+}
+a{
+  text-decoration: none;
+  border:none;
+}
+a img{
+  border:none 0px;
+}
+img{
+	border:none;
+}
+
+</style>
+<!--Fireworks CS6 Dreamweaver CS6 target.  Created Mon Oct 29 16:53:20 GMT+0900 2012-->
+</head>
+<body bgcolor="#ffffff">
+<div id="wrapper">
+<div id="contents">
+<table style="display: inline-table;" border="0" cellpadding="0" cellspacing="0" width="960">
+<!-- fwtable fwsrc="サイト.png" fwpage="ページ 1" fwbase="サイト.png" fwstyle="Dreamweaver" fwdocid = "1617941112" fwnested="0" -->
+  <tr>
+   <td><img src="images/spacer.gif" width="70" height="1" alt="" /></td>
+   <td><img src="images/spacer.gif" width="23" height="1" alt="" /></td>
+   <td><img src="images/spacer.gif" width="72" height="1" alt="" /></td>
+   <td><img src="images/spacer.gif" width="56" height="1" alt="" /></td>
+   <td><img src="images/spacer.gif" width="69" height="1" alt="" /></td>
+   <td><img src="images/spacer.gif" width="55" height="1" alt="" /></td>
+   <td><img src="images/spacer.gif" width="70" height="1" alt="" /></td>
+   <td><img src="images/spacer.gif" width="188" height="1" alt="" /></td>
+   <td><img src="images/spacer.gif" width="202" height="1" alt="" /></td>
+   <td><img src="images/spacer.gif" width="155" height="1" alt="" /></td>
+   <td><img src="images/spacer.gif" width="1" height="1" alt="" /></td>
+  </tr>
+
+  <tr>
+   <td colspan="10"><img name="n_r1_c1" src="images/index_r1_c1.png" width="960" height="196" id="n_r1_c1" alt="" /></td>
+   <td><img src="spacer.gif" width="1" height="196" alt="" /></td>
+  </tr>
+  <tr>
+   <td rowspan="6" colspan="2"><img name="n_r2_c1" src="images/index_r2_c1.png" width="93" height="541" id="n_r2_c1" alt="" /></td>
+   <td rowspan="2"><a href="http://web009.hotcom-web.com/"><img name="home_button" src="images/home_button.png" width="72" height="74" id="home_button" alt="" /></a></td>
+   <td><img name="n_r2_c4" src="images/index_r2_c4.png" width="56" height="28" id="n_r2_c4" alt="" /></td>
+   <td rowspan="2"><img name="plofile_button" src="images/plofile_button.png" width="69" height="74" id="plofile_button" alt="" /></td>
+   <td><img name="n_r2_c6" src="images/index_r2_c6.png" width="55" height="28" id="n_r2_c6" alt="" /></td>
+   <td rowspan="2"><a href="mailto:kanemoto.hisako.tci@gmail.com"><img name="contact_buttoon" src="images/contact_buttoon.png" width="70" height="74" id="contact_buttoon" alt="" /></a></td>
+   <td rowspan="4" colspan="3"><img name="n_r2_c8" src="images/index_r2_c8.png" width="545" height="429" id="n_r2_c8" alt="" /></td>
+   <td><img src="spacer.gif" width="1" height="28" alt="" /></td>
+  </tr>
+  <tr>
+   <td rowspan="2"><a href="http://web009.hotcom-web.com/wordpress/"><img name="blog_button" src="images/blog_button.png" width="56" height="61" id="blog_button" alt="" /></a></td>
+   <td rowspan="2"><img name="link_button" src="images/link_button.png" width="55" height="61" id="link_button" alt="" /></td>
+   <td><img src="spacer.gif" width="1" height="46" alt="" /></td>
+  </tr>
+  <tr>
+   <td rowspan="4"><img name="n_r4_c3" src="images/index_r4_c3.png" width="72" height="467" id="n_r4_c3" alt="" /></td>
+   <td rowspan="4"><img name="n_r4_c5" src="images/index_r4_c5.png" width="69" height="467" id="n_r4_c5" alt="" /></td>
+   <td rowspan="7"><img name="n_r4_c7" src="images/index_r4_c7.png" width="70" height="690" id="n_r4_c7" alt="" /></td>
+   <td><img src="spacer.gif" width="1" height="15" alt="" /></td>
+  </tr>
+  <tr>
+   <td rowspan="3"><img name="n_r5_c4" src="images/index_r5_c4.png" width="56" height="452" id="n_r5_c4" alt="" /></td>
+   <td rowspan="3"><img name="n_r5_c6" src="images/index_r5_c6.png" width="55" height="452" id="n_r5_c6" alt="" /></td>
+   <td><img src="spacer.gif" width="1" height="340" alt="" /></td>
+  </tr>
+  <tr>
+   <td rowspan="5"><img name="n_r6_c8" src="images/index_r6_c8.png" width="188" height="335" id="n_r6_c8" alt="" /></td>
+   <td><img name="click_button" src="images/click_button.png" width="202" height="86" id="click_button" alt="" /></td>
+   <td rowspan="5"><img name="n_r6_c10" src="images/index_r6_c10.png" width="155" height="335" id="n_r6_c10" alt="" /></td>
+   <td><img src="spacer.gif" width="1" height="86" alt="" /></td>
+  </tr>
+  <tr>
+   <td rowspan="4"><img name="n_r7_c9" src="images/index_r7_c9.png" width="202" height="249" id="n_r7_c9" alt="" /></td>
+   <td><img src="spacer.gif" width="1" height="26" alt="" /></td>
+  </tr>
+  <tr>
+   <td rowspan="3"><img name="n_r8_c1" src="images/index_r8_c1.png" width="70" height="223" id="n_r8_c1" alt="" /></td>
+   <td colspan="5"><a href="http://www.facebook.com/index.php?stype=lo&amp;lh=Ac_M2Id8M1Kjvyzr"><img name="facebook_button" src="images/facebook_button.png" width="275" height="89" id="facebook_button" alt="" /></a></td>
+   <td><img src="spacer.gif" width="1" height="89" alt="" /></td>
+  </tr>
+  <tr>
+   <td colspan="5"><img name="okinawa_button" src="images/okinawa_button.png" width="275" height="93" id="okinawa_button" alt="" /></td>
+   <td><img src="spacer.gif" width="1" height="93" alt="" /></td>
+  </tr>
+  <tr>
+   <td colspan="5"><img name="n_r10_c2" src="images/index_r10_c2.png" width="275" height="41" id="n_r10_c2" alt="" /></td>
+   <td><img src="spacer.gif" width="1" height="41" alt="" /></td>
+  </tr>
+</table>
+<div id="whats">What's New</div>
+<div id="blog">
+<ul>
+<?php
+$i = 0;
+$posts = get_posts("orderby=post_date&offset=0");
+foreach ($posts as $post):
+setup_postdata($post);
+?>
+<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+<?php endforeach; ?>
+</ul>
+</div>
+</div>
+</body>
+</html>
